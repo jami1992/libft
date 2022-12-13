cc2a9a438b615820caee12b6d9e66d1437a43ea0
Jan-Michael Druba <jdruba@c2s19d125.42wolfsburg.de>
2022-12-13 17:36:11 +0100

    added strncmp strrchr

---
+++ ft_strncmp.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdruba <jdruba@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/13 17:14:29 by jdruba            #+#    #+#             */
+/*   Updated: 2022/12/13 17:34:48 by jdruba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <stdio.h>
+// #include <string.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned long	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && i < n && s1[i] != '\0' && s2[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+// int	main(void)
+// {
+// 	char s1[] = "This it a string";
+// 	char s2[] = "This is a string";
+// 	unsigned long n;
+
+// 	n = 3;
+
+// 	if (strncmp(s1, s2, n) == 0)
+// 		printf("strings are equal\n");
+// 	else if (strncmp(s1, s2, n) > 0)
+// 		printf("s1 > s2\n");
+// 	else if (strncmp(s1, s2, n) < 0)
+// 		printf("s2 > s1\n");
+
+// if (ft_strncmp(s1, s2, n) == 0)
+// 		printf("strings are equal\n");
+// 	else if (ft_strncmp(s1, s2, n) > 0)
+// 		printf("s1 > s2\n");
+// 	else if (ft_strncmp(s1, s2, n) < 0)
+// 		printf("s2 > s1\n");
+// }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,23 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdruba <jdruba@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/13 10:36:12 by jdruba            #+#    #+#             */
-/*   Updated: 2022/12/13 13:44:15 by jdruba           ###   ########.fr       */
+/*   Created: 2022/12/13 13:45:29 by jdruba            #+#    #+#             */
+/*   Updated: 2022/12/13 14:14:59 by jdruba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <stdio.h>
 // #include <string.h>
 
-char	*ft_strchr(char *s, int c)
+// locates the last occurence of c in s
+char	*ft_strrchr(char *s, int c)
 {
-	while (*s != c)
+	while (*s != '\0')
 	{
 		s++;
+	}
+	while (*s != c)
+	{
+		s--;
 		if (*s == '\0' && c != '\0')
 			return (NULL);
 		if (*s == '\0' && c == '\0')
@@ -28,29 +33,31 @@ char	*ft_strchr(char *s, int c)
 
 // int main(void)
 // {
-// 	char tweet[] = "this is my @mention";
-// 	char *mention1 = strchr(tweet, '@');
-// 	char *mention2 = ft_strchr(tweet, '@');
+// 	char tweet[] = "this is my @menti@n";
+// 	char *mention1 = strrchr(tweet, '@');
+// 	char *mention2 = ft_strrchr(tweet, '@');
 
 // 	// /* returns a pointer to the position of @ so it 
 // 	// returns a substring */
 // 	printf("original from mention \t%s\n", mention1);
 // 	printf("my own from mention \t%s\n", mention2);
+
 // 	printf("addr of orig. mention %p\n", mention1);
-// 	printf("addr of orig. mention %p\n", tweet +11);
+// 	printf("addr of orig. mention %p\n", tweet + 17);
+
 // 	printf("addr of my mention %p\n", mention2);
-// 	printf("addr of my mention %p\n", tweet + 11);
+// 	printf("addr of my mention %p\n", tweet + 17);
 // 	// mention[0] = '+'; /*changes here change the original string, because 
 // 	// we are working with pointers!*/
 // 	// /*if the substring is not found in the string, NULL is returned:*/
-// 	if(strchr(tweet, '#') == NULL)
+// 	if(strrchr(tweet, '#') == NULL)
 // 		printf("original strchr returned\tNULL\n");
-// 	if(ft_strchr(tweet, '#') == NULL)
+// 	if(ft_strrchr(tweet, '#') == NULL)
 // 		printf("my own strchr returned\t\tNULL\n");
-// 	printf("address with original:\t%p\n", strchr(tweet, '@'));
-// 	printf("address with myown:\t%p\n", ft_strchr(tweet, '@'));
+// 	printf("address with original:\t%p\n", strrchr(tweet, '@'));
+// 	printf("address with myown:\t%p\n", ft_strrchr(tweet, '@'));
 
-// 	printf("addr of nulldeterm with original: \t%p\n", strchr(tweet, '\0'));
-// 	printf("addr of nulldeterm with my own: \t%p\n", ft_strchr(tweet, '\0'));
+// 	printf("addr of nulldeterm with original: \t%p\n", strrchr(tweet, '\0'));
+// 	printf("addr of nulldeterm with my own: \t%p\n", ft_strrchr(tweet, '\0'));
 // 	return (0);
 // }
