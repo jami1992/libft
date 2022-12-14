4c78d5493e4f708258bc5cc1ab144b47f591da43
Jan-Michael Druba <jdruba@c2s19d125.42wolfsburg.de>
2022-12-14 20:37:15 +0100

    added memset

---
+++ ft_memset.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdruba <jdruba@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/14 20:10:52 by jdruba            #+#    #+#             */
+/*   Updated: 2022/12/14 20:37:02 by jdruba           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include <string.h>
+// #include <stdio.h>
+
+void	*ft_memset(char *b, int c, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	if (b == 0)
+		return (0);
+	while (i < len)
+	{
+		*(unsigned char *)(b) = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
+
+// int	main(void)
+// {
+// 	char buffer[2];
+// 	printf("original\t%s\n", memset(buffer, 'a', 1));
+// 	printf("own\t\t%s\n", ft_memset(buffer, 'a', 1));
+// }
