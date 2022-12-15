4362624de5b9273f8515cfa4d82a09dddfc4e3fd
Jan-Michael Druba <jdruba@c2s19d125.42wolfsburg.de>
2022-12-15 10:51:16 +0100

    added ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,15 +6,60 @@
 /*   By: jdruba <jdruba@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 20:38:35 by jdruba            #+#    #+#             */
-/*   Updated: 2022/12/14 20:49:30 by jdruba           ###   ########.fr       */
+/*   Updated: 2022/12/15 10:50:55 by jdruba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+// #include <string.h>
+// #include <stdio.h>
 
-int	main(void)
+/*
+Thanks to this post, I found out, that you have to work with an extra variable
+you can not work directly with void *s
+https://stackoverflow.com/questions/29182966/do-you-have-a-simple-bzero-example
+*/
+void	ft_bzero(void *s, size_t n)
 {
-	char str1[] = "123456789";
-	printf("%s", bzero(str1, 5));
+	size_t	i;
+	char	*c;
+
+	c = s;
+	i = 0;
+	while (i < n)
+	{
+		c[i] = '\0';
+		i++;
+	}
 }
+
+/* for testing: a normal printf statement does not work, as bzero fills with
+NULL-Determinators.
+*/
+// int	main(void)
+// {
+// 	// original function:
+// 	char str1[] = "123456789";
+// 	bzero(str1, 4);
+// 	int	i;
+
+// 	i = 0;
+// 	while(i < 8)
+// 	{
+// 		printf("%d ", str1[i]);
+// 		i++;
+// 	}
+// 	puts(str1);
+
+// 	// testing own function:
+// 	char str2[] = "123456789";
+// 	ft_bzero(str2, 4);
+// 	int j;
+
+// 	j = 0;
+// 	while (j < 8)
+// 	{
+// 		printf("%d ", str2[j]);
+// 		j++;
+// 	}
+// 	puts(str2);
+// }
